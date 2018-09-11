63ad1a54fcaadc0ccd1560bdb2b1562e9c0538f3
boringlawyer <cnk5777@rit.edu>
2018-09-11 01:12:26 -0400

    Finished C03

--- C05_Shaders/AppClass.cpp
+++ C05_Shaders/AppClass.cpp
@@ -115,6 +115,8 @@ void AppClass::ProcessKeyboard(sf::Event a_event)
 		m_v3Color = glm::vec3(0.0f, 0.0f, 1.0f);
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num0))
 		m_v3Color = glm::vec3(-1.0f, -1.0f, -1.0f);
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4))
+		isCompColor = !isCompColor;
 }
 void AppClass::Display(void)
 {
@@ -123,8 +125,9 @@ void AppClass::Display(void)
 
 	//read uniforms and send values
 	GLuint SolidColor = glGetUniformLocation(m_uShaderProgramID, "SolidColor");
+	GLboolean shaderComp = glGetUniformLocation(m_uShaderProgramID, "comp");
+	glUniform1i(shaderComp, isCompColor);
 	glUniform3f(SolidColor, m_v3Color.r, m_v3Color.g, m_v3Color.b);
-
 	//draw content
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
