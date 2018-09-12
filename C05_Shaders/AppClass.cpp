479b3362b203e0796f8093119479faa81f712e40
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2018-09-11 23:39:44 -0400

    Committing to take in changes from origin

--- C05_Shaders/AppClass.cpp
+++ C05_Shaders/AppClass.cpp
@@ -115,6 +115,8 @@ void AppClass::ProcessKeyboard(sf::Event a_event)
 		m_v3Color = glm::vec3(0.0f, 0.0f, 1.0f);
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num0))
 		m_v3Color = glm::vec3(-1.0f, -1.0f, -1.0f);
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4))
+		m_v3Color = glm::vec3(-1.0f, -1.0f, -1.0f);
 }
 void AppClass::Display(void)
 {
