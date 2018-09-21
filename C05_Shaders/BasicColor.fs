10fcd6574c1e42e3526d22cabdf7987a9619dbf1
boringlawyer <cnk5777@rit.edu>
2018-09-21 17:02:58 -0400

    Trying to catch up to origin

--- C05_Shaders/BasicColor.fs
+++ C05_Shaders/BasicColor.fs
@@ -1,14 +1,18 @@
 #version 330
 
 in vec3 Color;
-
+ 
 uniform vec3 SolidColor = vec3(-1,-1,-1);
-
+uniform bool comp = false;
 out vec4 Fragment;
 
 void main()
 {
 	Fragment = vec4(Color,1);
+	if (comp)
+	{
+		Fragment = vec4(1 - Color.x, 1 - Color.y, 1 - Color.z, 1);
+	}
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
 		Fragment = vec4(SolidColor, 1);
